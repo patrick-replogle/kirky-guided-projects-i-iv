81468b344af156700790151a1f9f386ec23ce059
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-02-05 20:12:16 -0800

    Add files via upload

---
+++ node-api4-guided/api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const apiRouter = require('./api-router.js');
+
+const server = express();
+
+server.use(helmet());
+
+server.use('/api', apiRouter);
+
+module.exports = server;
---
+++ node-api4-guided/index.js
@@ -0,0 +1,6 @@
+const server = require('./api/server.js');
+
+const port = 4000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
